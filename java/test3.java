990452ac95898269dd90f2d105cbdeb36c480489
lihaimin666 <35094686@qq.com>
2020-06-20 16:22:25 +0800

    add lizi

---
+++ java/test3.java
@@ -0,0 +1,12 @@
+/**
+结合\n(换行)，\t(制表符)，空格等在控制台打印出如下图所示的效果
+*/
+
+public class test3{
+	
+	public static void main(String[] args){
+		
+		System.out.println("\t*\t\t\t*\t\n");
+		System.out.println("     *\t  *\t\t\t  *\t *  \n");
+	}
+}
