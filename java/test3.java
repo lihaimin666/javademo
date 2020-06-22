71ef1e0cccfb39fce52942ed704ecd79281aaedc
lihaimin666 <35094686@qq.com>
2020-06-22 09:35:10 +0800

    add

--- java/test3.java
+++ java/test3.java
@@ -1,5 +1,6 @@
 /**
-结合\n(换行)，\t(制表符)，空格等在控制台打印出如下图所示的效果
+结合\n(换行)，\t(制表符)，空格等在控制台打印出如下图所示的效果110  011   1100  0001    0011 1111   1100 0001
+
 */
 
 public class test3{
@@ -8,5 +9,10 @@ public class test3{
 		
 		System.out.println("\t*\t\t\t*\t\n");
 		System.out.println("     *\t  *\t\t\t  *\t *  \n");
+	}
+	
+	public  test(){
+		
+		
 	}
 }
