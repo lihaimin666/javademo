338efd8199dc3e7833f1ebc374d1ce3b0cbe0b3b
lihaimin666 <35094686@qq.com>
2020-06-27 11:39:44 +0800

    修改质数双重循环

--- src/org/test/xh/lizi2.java
+++ src/org/test/xh/lizi2.java
@@ -1,16 +1,19 @@
 package org.test.xh;
 
 public class lizi2 {
-
+/**
+ * 求 1~100之间的质数（质数是能被  1 和自身整除的数据）
+ * @param args
+ */
 	public static void main(String[] args) {
 		for (int i = 2; i <= 100; i++) {
 			boolean flag = true;
+			//如果2到自身减1 有能被整除的就不是质数
 			for (int j = 2; j < i; j++) {
 				if (i % j == 0) {
 					flag = false;
 					break;
 				}
-//				System.out.println("out");
 			}
 			if (flag) {
 				System.out.print(" " + i);
