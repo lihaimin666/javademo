dc6eb2da588cd68ef2c23369f5a08b415d320d5d
lihaimin666 <35094686@qq.com>
2020-06-27 11:36:53 +0800

    qian tao for

--- src/org/test/xh/lizi.java
+++ src/org/test/xh/lizi.java
@@ -5,19 +5,20 @@ public class lizi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int  result =0;
-		for(int i=1;i<=100;i++) {
-			
-			if(i%2==0) {
-				result+=i;
-			}
-		}
-		System.out.println("result = "+result);
+//		for(int i=1;i<=100;i++) {
+//			
+//			if(i%2==0) {
+//				result+=i;
+//			}
+//		}
+//		System.out.println("result = "+result);
 		
-		int j=1;
+		int j=2;
 		result =0;
 		while(j<=100) {
 			if(j%2==0) {
 				result+=j;
+//				j++;
 			}
 			j++;
 		}
